5c7c811bc3621a14bb79cdb52c58a87c088032fe
Daniel Kennett <daniel@kennettnet.co.uk>
2010-03-09 09:58:48 +0000

    Added KNViewController and KNWindowController.

---
+++ KNFoundation/KNViewController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.IO;
+using System.Reflection;
+using KNFoundation.KNKVC;
+
+namespace KNFoundation {
+
+    public class KNViewController {
+
+        private UserControl view;
+        private Object representedObject;
+
+
+        public KNViewController(string viewXamlName) {
+
+            try {
+
+
+
+                string xamlPath = KNBundle.MainBundle().PathForResourceOfType(viewXamlName, "xaml");
+                if (xamlPath != null) {
+
+                    FileStream s = new FileStream(xamlPath, FileMode.Open);
+                    DependencyObject rootElement = (DependencyObject)XamlReader.Load(s);
+                    UserControl newView = (UserControl)rootElement;
+                    View = newView;
+
+                    // Find our properties, then search for controls with the same name
+
+                    Type myType = GetType();
+                    PropertyInfo[] properties = myType.GetProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+                    foreach (PropertyInfo property in properties) {
+
+                        if (property.CanWrite) {
+
+                            Object item = LogicalTreeHelper.FindLogicalNode(rootElement, property.Name);
+                            if (item != null) {
+                                property.SetValue(this, item, null);
+                            }
+                        }
+                    }
+
+                    s.Dispose();
+
+                } else {
+                    throw new Exception(viewXamlName + " could not be found");
+                }
+
+            } catch {
+                throw;
+            }
+        }
+
+        #region "Properties"
+
+        public UserControl View {
+            get { return view; }
+            private set {
+                this.WillChangeValueForKey("View");
+                view = value;
+                this.DidChangeValueForKey("View");
+            }
+        }
+
+        public Object RepresentedObject {
+            get { return representedObject; }
+            set {
+                this.WillChangeValueForKey("RepresentedObject");
+                representedObject = value;
+                this.DidChangeValueForKey("RepresentedObject");
+            }
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ KNFoundation/KNWindowController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.IO;
+using System.Windows.Markup;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+using System.Windows.Media;
+
+using KNFoundation.KNKVC;
+
+namespace KNFoundation {
+
+    public class KNWindowController {
+        
+        private Window window;
+        private KNViewController rootViewController;
+
+        public KNWindowController(string windowXamlName) {
+
+            try {
+
+                string xamlPath = KNBundle.MainBundle().PathForResourceOfType(windowXamlName, "xaml");
+                if (xamlPath != null) {
+
+                    FileStream s = new FileStream(xamlPath, FileMode.Open);
+                    Object rootElement = (Object)XamlReader.Load(s);
+                    Window newWindow = (Window)rootElement;
+                    Window = newWindow;
+                    s.Dispose();
+
+                } else {
+                    throw new Exception(windowXamlName + " could not be found");
+                }
+
+            } catch {
+                throw;
+            }
+        }
+
+        public void ShowWindow() {
+            // We want to make sure the window has been rendered fully 
+            // before showing it. 
+
+            Window.Show();
+
+        }
+
+        #region "Properties"
+
+        public Window Window {
+            get { return window; }
+            private set {
+                this.WillChangeValueForKey("Window");
+                window = value;
+                this.DidChangeValueForKey("Window");
+            }
+        }
+
+        public KNViewController ViewController {
+            get { return rootViewController; }
+            set {
+                this.WillChangeValueForKey("ViewController");
+                rootViewController = value;
+                Window.Content = value.View;
+                this.DidChangeValueForKey("ViewController");
+            }
+        }
+
+
+
+        #endregion
+
+    }
+
+}
+
