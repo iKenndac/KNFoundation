78b2d9f8e50a924387937b1c42fd7e51e12805f4
Daniel Kennett <daniel@kennettnet.co.uk>
2010-03-24 17:36:04 +0000

    KNDependencyObjectKVC.ValueForKey() is now thread safe.

--- KNFoundation/KNKVC/KVC/KNDependencyObjectKVC.cs
+++ KNFoundation/KNKVC/KVC/KNDependencyObjectKVC.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Threading;
 
 namespace KNFoundation.KNKVC {
     static class KNDependencyObjectKVC {
 
-        public static Object ValueForKey(this DependencyObject o, String key) {
+        public static Object ValueForKey(this DependencyObject o, string key) {
+
+            if (o.Dispatcher.Thread == Thread.CurrentThread) {
+                return LogicalTreeHelper.FindLogicalNode(o, key);
+            } else {
+                return o.Dispatcher.Invoke(new FindNodeDelegate(FindNode), new object[] {o, key});
+            }
+        }
+
+        private delegate Object FindNodeDelegate(DependencyObject o, string key);
+
+        private static Object FindNode(DependencyObject o, string key) {
             return LogicalTreeHelper.FindLogicalNode(o, key);
         }
     }
