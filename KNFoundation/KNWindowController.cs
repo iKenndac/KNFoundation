c5b05303ac2530b82ffdc25fe3d2d534afb5f46d
Daniel Kennett <daniel@kennettnet.co.uk>
2010-05-28 14:59:32 +0100

    Added KNWindowController.SetWindowGlassMargins.

--- KNFoundation/KNWindowController.cs
+++ KNFoundation/KNWindowController.cs
@@ -17,6 +17,7 @@ namespace KNFoundation {
         
         private Window window;
         private KNViewController rootViewController;
+        private Thickness glassMargins = new Thickness(0);
 
         public KNWindowController(string windowXamlName) {
 
@@ -28,6 +29,7 @@ namespace KNFoundation {
                     FileStream s = new FileStream(xamlPath, FileMode.Open);
                     Object rootElement = (Object)XamlReader.Load(s);
                     Window newWindow = (Window)rootElement;
+                    newWindow.SourceInitialized += WindowWasInitialized;
                     Window = newWindow;
 
                     // Attempt to localise
@@ -56,7 +58,30 @@ namespace KNFoundation {
             // before showing it. 
 
             Window.Show();
+        }
 
+        public void SetWindowGlassMargins(Thickness thickness) {
+
+            glassMargins = thickness;
+
+            if (Window != null) {
+
+                if (Window.IsInitialized) {
+                    ExtendGlass(thickness);
+                }
+            }
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled) {
+            if (msg == WM_DWMCOMPOSITIONCHANGED) {
+                ExtendGlass(glassMargins);
+                handled = true;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void WindowWasInitialized(object sender, EventArgs e) {
+            ExtendGlass(glassMargins);
         }
 
         #region "Properties"
@@ -88,6 +113,67 @@ namespace KNFoundation {
 
 
         #endregion
+
+#region Glass
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MARGINS {
+            public int cxLeftWidth;
+            public int cxRightWidth;
+            public int cyTopHeight;
+            public int cyBottomHeight;
+        }
+
+        private const int WM_DWMCOMPOSITIONCHANGED = 0x031E;
+
+        [DllImport("dwmapi.dll")]
+        static extern int
+           DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS pMarInset);
+
+        [DllImport("dwmapi.dll")]
+        extern static int DwmIsCompositionEnabled(ref int en);
+
+        private void ExtendGlass(Thickness thickness) {
+            try {
+                int isGlassEnabled = 0;
+                DwmIsCompositionEnabled(ref isGlassEnabled);
+                if (Environment.OSVersion.Version.Major > 5 && isGlassEnabled > 0) {
+                    // Get the window handle
+                    WindowInteropHelper helper = new WindowInteropHelper(window);
+
+                    if (helper.Handle != IntPtr.Zero) {
+
+                        HwndSource mainWindowSrc = (HwndSource)HwndSource.FromHwnd(helper.Handle);
+                        mainWindowSrc.CompositionTarget.BackgroundColor = Colors.Transparent;
+
+                        // Set Margins
+
+                        // Get the dpi of the screen
+                        System.Drawing.Graphics desktop = System.Drawing.Graphics.FromHwnd(mainWindowSrc.Handle);
+                        float dpiX = desktop.DpiX / 96;
+                        float dpiY = desktop.DpiY / 96;
+
+                        MARGINS margins = new MARGINS() { cxLeftWidth = 0, cxRightWidth = 0, cyBottomHeight = 0, cyTopHeight = 0 };
+                        margins.cxLeftWidth = (int)(thickness.Left * dpiX);
+                        margins.cxRightWidth = (int)(thickness.Right * dpiX);
+                        margins.cyBottomHeight = (int)(thickness.Bottom * dpiY);
+                        margins.cyTopHeight = (int)(thickness.Top * dpiY);
+
+                        window.Background = Brushes.Transparent;
+
+                        int hr = DwmExtendFrameIntoClientArea(mainWindowSrc.Handle, ref margins);
+
+                    }
+
+                } else {
+                    window.Background = SystemColors.WindowBrush;
+                }
+            } catch (DllNotFoundException) {
+
+            }
+        }
+
+#endregion
 
     }
 
